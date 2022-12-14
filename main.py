2df0101744abdd193facc7b030e4cf4f3793d96e
Gurkirat Singh <gurkirat63@gmail.com>
2022-12-14 16:34:11 +1100

    fetching json content from api to load title and subtitles for blog using jinja

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    data_list = requests.get(url="https://api.npoint.io/4af156202f984d3464c3").json()
+
+
+    return render_template("index.html", data=data_list)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8001)
